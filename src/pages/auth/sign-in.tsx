daaadff8d014d2f792ae565d79aebd6396e050df
unknown <aurelio.chagas>
2024-03-04 17:32:13 -0300

    botão para registrar

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,6 @@
-import { Loader2 } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -23,19 +23,17 @@ export function SignIn() {
 
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
-
-      // throw new Error()
-
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
-      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
           label: 'Reenviar',
-          onClick: () => handleSignIn(data),
+          onClick: () => {
+            handleSignIn(data)
+          },
         },
       })
-    } catch {
+    } catch (error) {
       toast.error('Credenciais inválidas.')
     }
   }
@@ -43,28 +41,30 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
+
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Acesse o painel
+              Acessar painel
             </h1>
             <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas
+              Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
-            <div className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
-            <Button className="w-full" type="submit" disabled={isSubmitting}>
-              {isSubmitting ? (
-                <Loader2 className="animate-spin" />
-              ) : (
-                'Acessar painel'
-              )}
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar painel
             </Button>
           </form>
         </div>
